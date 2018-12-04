a4583a5634bef53b6798137fdeddc2d48bc82045
Niko Virolainen <niko.virolainen@hotmail.com>
2018-12-04 12:47:57 +0200

    backgroung image

--- Home.js
+++ Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image, Alert, TextInput, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, ImageBackground, Alert, TextInput, FlatList } from 'react-native';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import { SQLite } from 'expo';
 
@@ -61,42 +61,47 @@ export default class App extends React.Component {
 
     const {navigate} = this.props.navigation;
 
+    
+
     return (
-      <View style={styles.container}>
+         
+    <ImageBackground source={require('./backgroundphotos/ice-hockey-photo.jpg')} style={styles.imgBackground}> 
+        <View>
+      
         
-        <Text style={{fontStyle: 'italic', fontWeight: 'bold', fontSize: 20, textDecorationLine: 'underline'}}>Nimi:</Text>
-        <TextInput placeholder='Nimi' style={{ marginTop: 5, marginBottom: 5,  fontSize:25, width: 300, height: 30, borderColor: 'gray', borderWidth: 1}}
+        <Text style={{color: 'white', fontStyle: 'italic', fontWeight: 'bold', fontSize: 20, textDecorationLine: 'underline'}}>Nimi:</Text>
+        <TextInput placeholder='Nimi' style={{ marginTop: 5, marginBottom: 5,  fontSize:25, width: 300, height: 30, borderColor: 'black', borderWidth: 1}}
           onChangeText={(name) => 
           this.setState({name})}
           value={this.state.name}/>
 
-        <Text>Position:</Text>
-        <TextInput placeholder='Position' style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        <Text style={{color: 'white', fontStyle: 'italic', fontWeight: 'bold', fontSize: 20, textDecorationLine: 'underline'}}>Position: (G/D/F)</Text>
+        <TextInput placeholder='Position' style={{ marginTop: 5, marginBottom: 5,  fontSize:25, width: 300, height: 30, borderColor: 'black', borderWidth: 1}}
           onChangeText={(position) => 
           this.setState({position})}
           value={this.state.position}/> 
 
-        <Text>Pelatut Ottelut:</Text>
-        <TextInput placeholder='Pelatut Ottelut' keyboardType="numeric" style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        <Text style={{color: 'white', fontStyle: 'italic', fontWeight: 'bold', fontSize: 20, textDecorationLine: 'underline'}}>Pelatut Ottelut:</Text>
+        <TextInput placeholder='Pelatut Ottelut' keyboardType="numeric" style={{ marginTop: 5, marginBottom: 5,  fontSize:25, width: 300, height: 30, borderColor: 'black', borderWidth: 1}}
           onChangeText={(gamesPlayed) => 
           this.setState({gamesPlayed})}
           value={this.state.gamesPlayed}/>
 
-        <Text>Maalit:</Text>
-        <TextInput placeholder='Maalit' keyboardType="numeric" style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        <Text style={{color: 'white', fontStyle: 'italic', fontWeight: 'bold', fontSize: 20, textDecorationLine: 'underline'}}>Maalit:</Text>
+        <TextInput placeholder='Maalit' keyboardType="numeric" style={{ marginTop: 5, marginBottom: 5,  fontSize:25, width: 300, height: 30, borderColor: 'black', borderWidth: 1}}
           onChangeText={(goals) =>
           this.setState({goals})}
           value={this.state.goals}/>
 
-        <Text>Syötöt:</Text>
-        <TextInput placeholder='Syötöt' keyboardType="numeric" style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        <Text style={{color: 'white', fontStyle: 'italic', fontWeight: 'bold', fontSize: 20, textDecorationLine: 'underline'}}>Syötöt:</Text>
+        <TextInput placeholder='Syötöt' keyboardType="numeric" style={{ marginTop: 5, marginBottom: 5,  fontSize:25, width: 300, height: 30, borderColor: 'black', borderWidth: 1}}
           onChangeText={(assists) => 
           this.setState({assists})}
           value={this.state.assists}/>
+        
+        <Button onPress={this.savePlayer} color="green" title="Save player to database"/>
 
-          <Button onPress={this.savePlayer} color="green" title="Save player to database"/>
-
-              <Text style={{marginTop: 15, fontSize: 20}}>Player List:</Text>
+              <Text style={{textAlign: 'center', marginTop: 15, fontSize: 20}}>Player List:</Text>
               <FlatList 
                 style={{marginLeft: '5%', marginTop: '2%'}} 
                 keyExtractor={item => item.id.toString()} 
@@ -104,29 +109,34 @@ export default class App extends React.Component {
 
               <View style = {styles.listcontainer}>
               <Text style={{fontSize: 18}}>Nimi: {item.name}, Position: {item.position}, Pelatut Ottelut: {item.gamesPlayed}, Maalit: {item.goals}, Syötöt: {item.assists}, Pisteet: {item.goals + item.assists} </Text>
-              <Text style ={{color: 'black'}} onPress= {()  => this.deletePlayer(item.id)}>Poista pelaaja</Text></View>}
+              <Text style ={{color: 'black'}} onPress= {()  => this.deletePlayer(item.id)}>Delete player</Text></View>}
               data={this.state.players} ItemSeparatorComponent={this.listSeparator}
               />
               <Button color="green" onPress={() => navigate('Stats')} title="Stats" />
               </View>
+    </ImageBackground>
     );
   }
   }
 
 
   const styles = StyleSheet.create({
-    container: {
-      flex: 3,
-      flexDirection: 'column',
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: '15%'
+
+    imgBackground: {
+        flex: 3,
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%'
     },
+
     listcontainer: {
       flex: 1,
       flexDirection: 'column',
-      backgroundColor: 'pink',
-      alignItems: 'center'
+      backgroundColor: 'white',
+      alignItems: 'center',
+      marginLeft: '0%'
     }  
   });
